f0843d34df3e86d43b25240424efc26384562e39
jcarles <jcarles@kitchenmaster.es>
2016-07-05 15:12:41 +0200

    Creació toolbar.

--- app/src/main/java/com/jocajica/gotocar/MainActivity.java
+++ app/src/main/java/com/jocajica/gotocar/MainActivity.java
@@ -1,13 +1,26 @@
 package com.jocajica.gotocar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.container)
+    ViewPager container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
     }
 }
