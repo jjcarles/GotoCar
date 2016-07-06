ffa0b251f544365958342e6279287eba4b059ad7
jcarles <jcarles@kitchenmaster.es>
2016-07-06 15:39:03 +0200

    Creació toolbar.

--- app/src/main/java/com/jocajica/gotocar/MainActivity.java
+++ app/src/main/java/com/jocajica/gotocar/MainActivity.java
@@ -1,19 +1,17 @@
 package com.jocajica.gotocar;
 
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.widget.FrameLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import layout.MainMenuFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
-    @BindView(R.id.container)
-    ViewPager container;
+    @BindView(R.id.fragment_container)
+    FrameLayout fragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        setSupportActionBar(toolbar);
+        if (fragmentContainer != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            MainMenuFragment mainMenuFragment = new MainMenuFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container, mainMenuFragment)
+                    .commit();
+        }
     }
 }
