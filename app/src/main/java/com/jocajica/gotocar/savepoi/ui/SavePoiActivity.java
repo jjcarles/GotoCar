1f9c421313fdcd158dace55c30f173a578855b53
Joan Carles Jimenez <jjcarles@jocajica.com>
2016-07-10 12:41:21 +0200

    Estructura SavePoi

--- app/src/main/java/com/jocajica/gotocar/savepoi/ui/SavePoiActivity.java
+++ app/src/main/java/com/jocajica/gotocar/savepoi/ui/SavePoiActivity.java
@@ -1,20 +1,141 @@
 package com.jocajica.gotocar.savepoi.ui;
 
+import android.Manifest;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.jocajica.gotocar.GotoCarApp;
 import com.jocajica.gotocar.R;
+import com.jocajica.gotocar.savepoi.SavePoiPresenter;
+import com.jocajica.gotocar.savepoi.ui.adapters.SavePoiSectionsPagerAdapter;
+
+import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SavePoiActivity extends AppCompatActivity {
+public class SavePoiActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    @BindView(R.id.appbar)
+    AppBarLayout appbar;
+    @BindView(R.id.viewPager)
+    ViewPager viewPager;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.tabs)
+    TabLayout tabLayout;
+    @BindView(R.id.main_content)
+    CoordinatorLayout mainContent;
+
+    @Inject
+    SavePoiPresenter presenter;
+    @Inject
+    SavePoiSectionsPagerAdapter adapter;
+    @Inject
+    SharedPreferences sharedPreferences;
+
+    private Location lastLocation;
+    private GoogleApiClient googleApiClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_savepoi);
+        ButterKnife.bind(this);
+
+        setupInjection();
+        setupNavigation();
+        setupGoogleAPIClient();
+
+        presenter.onCreate();
+    }
+
+    @Override
+    protected void onDestroy() {
+        presenter.onDestroy();
+        super.onDestroy();
+    }
+
+
+    @Override
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    private void setupGoogleAPIClient() {
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    private void setupNavigation() {
+        toolbar.setTitle("Goto Car");
+        setSupportActionBar(toolbar);
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupInjection() {
+        String[] titles = new String[]{
+                getString(R.string.savepoi_title_details),
+                getString(R.string.savepoi_title_map)};
+
+        Fragment[] fragments = new Fragment[]{ new PhotoListFragment(),
+                new PhotoMapFragment()};
+
+        GotoCarApp app = (GotoCarApp) getApplication();
+        app.getMainComponent(this, getSupportFragmentManager(), fragments, titles).inject(this);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            lastLocation = null;
+            return;
+        }
+
+        lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Snackbar.make(viewPager, connectionResult.getErrorMessage(), Snackbar.LENGTH_SHORT).show();
     }
 }
